bfdf7d8af05c92af6030b6123905601c0192ede9
jebin shaju <67820811+jebinshaju@users.noreply.github.com>
2022-11-13 11:02:46 +0530

    Add files via upload

--- arrayop.c
+++ arrayop.c
@@ -1,31 +1,39 @@
 #include<stdio.h>
-
-int insert(int arr[],int n,int size)
+int size=30,arr[30];
+int insert(int n,int size)
 {
 	int loc,item,i;
 	if (n==size)
 	{
 		printf("\nThe array is full.\n");
+		return n;
 	}
 	else
-
 	{
 		printf("\nEnter the location(index) to insert: ");
 		scanf("%d",&loc);
-		printf("\nEnter the element to insert: ");
-		scanf("%d",&item);		
-		for (i=n-1;i>=loc;i--)
+		if(loc<=n-1)
 		{
-			arr[i+1]=arr[i];
+			printf("\nEnter the element to insert: ");
+			scanf("%d",&item);		
+			for (i=n-1;i>=loc;i--)
+			{
+				arr[i+1]=arr[i];
+			}
+			arr[loc]=item;
+			printf("Succesfully inserted %d",item);
+			n=n+1;
+			return n;
 		}
-		arr[loc]=item;
-		
-	}
-	n=n+1;
-	return n;
+		else
+		{
+			printf("Invalid location");
+			return n;
+		}
+	}	
 }
 
-int delete(int arr[],int n)
+int delete(int n)
 {
 	int item,i,j,flag=0;
 	printf("\nEnter the element to delete: ");
@@ -46,16 +54,22 @@ int delete(int arr[],int n)
 
 		}
 		n=n-1;
+		printf("Succesfully deleted %d",item);
 		return n;
 	}
 	else
+	{
 		printf("Element not found,");
+		return n;
+	}
+		
 	
 }
 
-void display(int arr[],int n)
+void display(int n)
 {
 	int i;
+	printf("");
 	for (i=0;i<n;i++)
 	{
 		printf("%d\n",arr[i]);
@@ -64,8 +78,10 @@ void display(int arr[],int n)
 
 int main()
 {
-	int n,arr[30],i,flag=1,size=30,op;
-
+	int n,i,flag=1,op;
+	printf("\nEnter the size of array: ");
+	scanf("%d",&size);
+	printf("\nInitialise the array...\n");
 	printf("\nEnter the number of elements of array: ");
 	scanf("%d",&n);
 	for(i=0;i<n;i++)
@@ -80,15 +96,15 @@ int main()
 		scanf("%d",&op);
 		if (op==1)
 		{
-			n=insert(arr,n,size);
+			n=insert(n,size);
 		}
 		else if (op==2)
 		{
-			n=delete(arr,n);
+			n=delete(n);
 		}
 		else if (op==3)
 		{
-			display(arr,n);
+			display(n);
 		}
 		else if (op==4)
 		{
@@ -99,6 +115,7 @@ int main()
 			printf("Invalid option\n");
 	}	
 	return 0;	
+	
 }
 
 
